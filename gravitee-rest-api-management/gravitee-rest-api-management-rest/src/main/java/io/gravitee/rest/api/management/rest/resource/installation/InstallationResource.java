gravitee-lab/zips-on-s3-gravitee-management-rest-api

feat(cockpit): manage cockpit installation registration
* creation of an *Installation* in database
* creation of a command producer to register to cockpit
* creation of an API resource to fetch the installation information

Closes gravitee-io/issues#4766

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/installation/InstallationResource.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.rest.resource.installation;
+
+import io.gravitee.common.http.MediaType;
+import io.gravitee.rest.api.management.rest.security.Permission;
+import io.gravitee.rest.api.management.rest.security.Permissions;
+import io.gravitee.rest.api.model.InstallationEntity;
+import io.gravitee.rest.api.model.permissions.RolePermission;
+import io.gravitee.rest.api.service.InstallationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import static io.gravitee.rest.api.model.permissions.RolePermissionAction.READ;
+
+/**
+ * Defines the REST resources to manage Portal.
+ *
+ * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Api(tags = {"Console"})
+@Path("/installation")
+public class InstallationResource {
+
+    @Inject
+    private InstallationService installationService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Get installation information",
+            notes = "User must have the INSTALLATION[READ] permission on the platform")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Installation definition", response = InstallationEntity.class),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
+    @Permissions({
+            @Permission(value = RolePermission.ORGANIZATION_SETTINGS, acls = { READ })
+    })
+    public Response getInstallation() {
+        return Response.ok(installationService.get()).build();
+    }
+}

gravitee-io/gravitee-management-rest-api

gravitee-rest-api-management/gravitee-rest-api-management-rest/src/main/java/io/gravitee/rest/api/management/rest/resource/installation/InstallationResource.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.rest.api.management.rest.resource.installation;
+
+import io.gravitee.common.http.MediaType;
+import io.gravitee.rest.api.management.rest.security.Permission;
+import io.gravitee.rest.api.management.rest.security.Permissions;
+import io.gravitee.rest.api.model.InstallationEntity;
+import io.gravitee.rest.api.model.permissions.RolePermission;
+import io.gravitee.rest.api.service.InstallationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import static io.gravitee.rest.api.model.permissions.RolePermissionAction.READ;
+
+/**
+ * Defines the REST resource to get installation information.
+ *
+ * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Api(tags = {"Console"})
+@Path("/installation")
+public class InstallationResource {
+
+    @Inject
+    private InstallationService installationService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Get installation information",
+            notes = "User must have the ORGANIZATION_INSTALLATION[READ] permission on the platform")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Installation definition", response = InstallationEntity.class),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
+    @Permissions({
+            @Permission(value = RolePermission.ORGANIZATION_INSTALLATION, acls = { READ })
+    })
+    public Response getInstallation() {
+        return Response.ok(installationService.get()).build();
+    }
+}